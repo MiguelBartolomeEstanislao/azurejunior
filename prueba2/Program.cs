b2fc51e98b7256eb657a79b07440265a015f7467
Miguel Bartolome Estanislao <miguelbartolomeestanislao@gmail.com>
2024-09-11 11:36:49 +0200

    Pruebas con variables

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,3 +1,21 @@
-﻿Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
+﻿string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write( messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius. ");
