c15e3b8a31414b6019368acfd655aa6c45e5596f
Miguel Bartolome Estanislao <miguelbartolomeestanislao@gmail.com>
2024-09-16 12:37:23 +0200

    Declaracion de matrices

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,31 +1,17 @@
+// B123
+// C234
+// A345
+// C15
+// B177
+// G3003
+//C235
+// B179
 
-// Random random = new Random();
-// var daysUntilExpiration = random.Next(12);
-// var discountPercentage = 20;
-
-//Your subscription will expire soon. Renew now!
-//Your subscription expires in _ days.
-//Your subscription expires within a day! Renew now and save 20%!
-//Your subscription has expired.
-
-//Console.Clear();
-
-//Console.WriteLine($"Days to expiration: {daysUntilExpiration}");
-
-//if (daysUntilExpiration > 10)
-{ 
-    // Console.WriteLine("Your subscription will expire soon. Renew now!");
+string [] orderIDS = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+foreach (string orderID in orderIDS)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
 }
-
-//else if (daysUntilExpiration <=5 && daysUntilExpiration > 1)
-{ 
-//    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days");
-}
-//lse if (daysUntilExpiration == 1)
-{ 
-    // Console.WriteLine($"Your subscription expires within a day! Renew now and save {discountPercentage}%!");
-}
-
-//else //
-// { Console.WriteLine("Your subscription has expired");
-    
