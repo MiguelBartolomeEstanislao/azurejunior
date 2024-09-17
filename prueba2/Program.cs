e6222ecab7bedd31fc3c7dcaffcceaa1afde100a
Miguel Bartolome Estanislao <miguelbartolomeestanislao@gmail.com>
2024-09-17 12:31:20 +0200

    switch

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -8,44 +8,50 @@ string type = "";
 string color = "";
 string size = "";
 
-if (product[0] == "01")
+switch (product[0])
 {
-    type = "Sweat shirt";
-} else if (product[0] == "02")
-{
-    type = "T-Shirt";
-} else if (product[0] == "03")
-{
-    type = "Sweat pants";
-}
-else
-{
-    type = "Other";
-}
-
-if (product[1] == "BL")
-{
-    color = "Black";
-} else if (product[1] == "MN")
-{
-    color = "Maroon";
-} else
-{
-    color = "White";
+    case "01":
+        type = "Sweat Shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
 }
 
-if (product[2] == "S")
+// Switch para el color
+switch (product[1])
 {
-    size = "Small";
-} else if (product[2] == "M")
-{
-    size = "Medium";
-} else if (product[2] == "L")
-{
-    size = "Large";
-} else
-{
-    size = "One Size Fits All";
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
 }
 
-Console.WriteLine($"Product: {size} {color} {type}");
+// Switch para el tamaño
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+        Console.WriteLine($"Product: {size} {color} {type}");
