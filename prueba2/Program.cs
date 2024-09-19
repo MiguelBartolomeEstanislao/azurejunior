b968cc6fd46c1d86d847aa9524d305629e27e5bd
Miguel Bartolome Estanislao <miguelbartolomeestanislao@gmail.com>
2024-09-19 14:00:01 +0200

    Tarea

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,15 +1,28 @@
-string [] values = {"12.3", "45", "ABC", "11", "DEF"};
-var mensaje = "";
-var total = 0.0;
-var resultado = 0.0;
-
-foreach (var value in values) 
+/* string pangram = "The quick brown fox jumps over the lazy dog";
+string [] message = pangram.Split();
+string [] newMessage = new string [message.Length];
+for (int i = 0; i < message.Length; i++)
 {
-if (double.TryParse(value, out resultado)) 
-{ total += resultado; }
-else
-{ mensaje += value;}
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+
+}
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result); */
+
+
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string [] items = orderStream.Split(',');
+
+Array.Sort(items);
+foreach (var item in items)
+{
+    if item (item.Length == 4)
+    Console.WriteLine(orderStream);
 }
 
-Console.WriteLine("Mensaje" + mensaje);
-Console.WriteLine("Total" + total);
+
+
+
