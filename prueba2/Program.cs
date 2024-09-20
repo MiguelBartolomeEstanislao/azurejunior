22153b075a8d181604d367767227fad94e0496c5
Miguel Bartolome Estanislao <miguelbartolomeestanislao@gmail.com>
2024-09-20 11:51:17 +0200

    Interpolacion de cadenas

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,27 +1,36 @@
-/* string pangram = "The quick brown fox jumps over the lazy dog";
-string [] message = pangram.Split();
-string [] newMessage = new string [message.Length];
-for (int i = 0; i < message.Length; i++)
-{
-    char[] letters = message[i].ToCharArray();
-    Array.Reverse(letters);
-    newMessage[i] = new string(letters);
+string customerName = "Ms. Barros";
+string currentProduct = "Magic Yield";
 
-}
-string result = String.Join(" ", newMessage);
-Console.WriteLine(result); */
+int currentShares = 2975000;
+string currentSharesFormatted = currentShares.ToString("N0");
 
 
+decimal currentReturn = 0.1275m;
+decimal currentReturnFormatted = currentReturn * 100;
+string currentReturnFormattedString = currentReturnFormatted.ToString("F2");
 
-string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-string [] items = orderStream.Split(',');
+decimal currentProfit = 55000000.0m;
+string newProduct = "Glorious Future";
 
-Array.Sort(items);
-foreach (var item in items)
-{
-    if item (item.Length == 4)
-    Console.WriteLine(orderStream);
-}
+decimal newReturn = 0.13125m;
+decimal newReturnFormatted = newReturn * 100;
+string newReturnFormattedString = currentReturnFormatted.ToString("F2");
+
+decimal newProfit = 63000000.0m;
+string formattedProfit = newProfit.ToString("C2");
+
+Console.Clear();
+
+string saludo = ($"Dear {customerName},");
+Console.WriteLine(saludo);
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return. \n");
+Console.WriteLine($"Currently, you own {currentSharesFormatted} shares at a return of {currentReturnFormattedString}% \n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturnFormattedString}.  Given your current volume, your potential profit would be {formattedProfit}. \n");
+Console.WriteLine("Here's a quick comparison:\n");
+Console.WriteLine($"{currentProduct, -20} {currentReturn:P2} {currentProfit,20:C2}");
+Console.WriteLine($"{newProduct, -20} {newReturn:P2} {newProfit,20:C2}");
+
+
 
 
 
